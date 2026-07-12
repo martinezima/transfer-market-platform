74a917c6d342075ed611ac69faec717b3e404131
Miguel Martinez Ibarra <martinezi.miguel@gmail.com>
2026-07-12 11:12:43 -0600

    US-0001 Add the rest of CRUD operations

--- src/WebApi/Controllers/PlayersController.cs
+++ src/WebApi/Controllers/PlayersController.cs
@@ -23,5 +23,79 @@ namespace TransferMarketPlatform.WebApi.Controllers
             var players = await _playerService.GetAllAsync(cancellationToken);
             return Ok(players);
         }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult<PlayerDto>> GetById(
+            Guid id,
+            CancellationToken cancellationToken = default
+        )
+        {
+            var player = await _playerService.GetByIdAsync(id, cancellationToken);
+            return player is null ? NotFound() : Ok(player);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PlayerDto>> Create(
+            [FromBody] CreatePlayerDto playerDto,
+            CancellationToken cancellationToken = default
+        )
+        {
+            try
+            {
+                var createdPlayer = await _playerService.CreateAsync(playerDto, cancellationToken);
+                return CreatedAtAction(
+                    nameof(GetAll),
+                    new { id = createdPlayer.Id },
+                    createdPlayer
+                );
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id:Guid}")]
+        public async Task<ActionResult<PlayerDto>> Update(
+            Guid id,
+            [FromBody] UpdatePlayerDto playerDto,
+            CancellationToken cancellationToken = default
+        )
+        {
+            try
+            {
+                var updatedPlayer = await _playerService.UpdateAsync(
+                    id,
+                    playerDto,
+                    cancellationToken
+                );
+                return Ok(updatedPlayer);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id:Guid}")]
+        public async Task<ActionResult<bool>> Delete(
+            Guid id,
+            CancellationToken cancellationToken = default
+        )
+        {
+            try
+            {
+                var deleted = await _playerService.DeleteAsync(id, cancellationToken);
+                return deleted ? NoContent() : NotFound();
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
